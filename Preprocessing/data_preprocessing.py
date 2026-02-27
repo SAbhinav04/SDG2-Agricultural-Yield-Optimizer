9dfdc1148c598cf500a9ec03c0d542d9fbfe9c39
Ananya H V <ananyahv214@gmail.com>
2026-02-27 15:00:18 +0530

    Applied StandardScaler

--- Preprocessing/data_preprocessing.py
+++ Preprocessing/data_preprocessing.py
@@ -2,7 +2,7 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler 
 
-df = pd.read_csv("D:\\iimstc\\crop_yield_pred\\SDG2-Agricultural-Yield-Optimizer\\Dataset\\crop_yield_with_crops.csv")
+df = pd.read_csv("C:\Users\Vijay kumar\Downloads\crop_yield_with_crops.csv")
 
 print(df.isnull().sum())
 print(df.duplicated().sum())
@@ -13,11 +13,9 @@ X = df.drop("Yield", axis=1)
 y = df["Yield"]
 X = pd.get_dummies(X, columns=['Crop'], drop_first=True) 
 
-
-
 # member 3
-
-
+scaler = StandardScaler() 
+X_scaled = scaler.fit_transform(X)
 
 # member 4
 
