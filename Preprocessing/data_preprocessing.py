fd5b618968c54978b320190a91efef614ee1daf8
shifa172004 <shifafirdose17@gmail.com>
2026-02-27 13:00:45 +0530

    Added data loading and inspection step

---
+++ Preprocessing/data_preprocessing.py
@@ -0,0 +1,20 @@
+# member 1
+import pandas as pd
+from sklearn.preprocessing import StandardScaler 
+
+df = pd.read_csv("D:\\iimstc\\crop_yield_pred\\SDG2-Agricultural-Yield-Optimizer\\Dataset\\crop_yield_with_crops.csv")
+
+print(df.isnull().sum())
+print(df.duplicated().sum())
+print(df.head())
+
+# member 2
+
+
+
+# member 3
+
+
+
+# member 4
+
